104210b5a4a4ff3dc7615e866a3de93d6287e29e
Ravi <2018mravi@gmail.com>
2023-01-30 14:47:23 +0530

    edit profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,4 @@
-/*
-  Authors : bunchdevelopers (Rahul Jograna)
-  Website : https://bunchdevelopers.com/
-  App Name : ionic6Template Pack
-  This App Template Source code is licensed as per the
-  terms found in the Website https://bunchdevelopers.com/license
-  Copyright and Good Faith Purchasers © 2021-present bunchdevelopers.
-*/
+
 import { Component, NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -18,32 +11,30 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
-
- 
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   },
   {
-    path: 'onboard',
-    loadChildren: () => import('./pages/onboard/onboard.module').then( m => m.OnboardPageModule)
+    path: 'edit-profile',
+    loadChildren: () => import('./pages/edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
   },
+
   {
     path: 'welcome',
     loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)
   },
-
   {
-    path: 'profile-setup',
-    loadChildren: () => import('./pages/profile-setup/profile-setup.module').then( m => m.ProfileSetupPageModule)
+    path: 'setting',
+    loadChildren: () => import('./pages/setting/setting.module').then( m => m.SettingPageModule)
+  },
+  {
+    path: 'booking',
+    loadChildren: () => import('./pages/appoinments/appoinments.module').then(m => m.AppoinmentsPageModule)
   },
 
  
 
-  {
-    path: 'friends-profile',
-    loadChildren: () => import('./pages/friends-profile/friends-profile.module').then( m => m.FriendsProfilePageModule)
-  },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
--- src/app/components/tinder-card/tinder-card.component.ts
+++ src/app/components/tinder-card/tinder-card.component.ts
@@ -1,11 +1,4 @@
-/*
-  Authors : bunchdevelopers (Rahul Jograna)
-  Website : https://bunchdevelopers.com/
-  App Name : ionic6Template Pack
-  This App Template Source code is licensed as per the
-  terms found in the Website https://bunchdevelopers.com/license
-  Copyright and Good Faith Purchasers © 2021-present bunchdevelopers.
-*/
+ 
 import { Component, ElementRef, Input, OnInit, Output, QueryList, Renderer2, ViewChildren, EventEmitter } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
